f11aceb83aa2fd7a660dcc0c3da0911d08f0d830
Bob Torgerson <rltorgerson@alaska.edu>
2016-08-03 16:54:13 -0800

    Update mapventure script to properly install MapVenture into its production location.

--- update_mapventure.sh
+++ update_mapventure.sh
@@ -2,9 +2,15 @@
 
 export GEONODE_URL="http://mapventure.iarc.uaf.edu:8000"
 export GEOSERVER_URL="http://mapventure.iarc.uaf.edu:8080/geoserver"
-export MV_LEAFLET_IMAGE_PATH="images/"
+export MV_LEAFLET_IMAGE_PATH="bower_components/leaflet/dist/images/"
+export MAPVENTURE_DIST="/var/www/mapventure-dist"
+
 BRANCH_NAME=$1
 
 git clone -b $BRANCH_NAME https://github.com/ua-snap/mapventure.git && cd mapventure
 
 npm install && grunt build --dist
+
+sudo rm -rf /tmp/mapventure-dist && sudo mv $MAPVENTURE_DIST /tmp/mapventure-dist && sudo cp -r dist $MAPVENTURE_DIST && sudo chown -R www-data:www-data $MAPVENTURE_DIST
+
+sudo service apache2 restart 
