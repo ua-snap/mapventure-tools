b4940b52299ec8152e7f66d73e02d279d0f048b3
Craig Stephenson <crstephenson@alaska.edu>
2016-10-31 12:43:54 -0800

    Import three most recent Barrow sea ice layers, and separate steps into functions.

--- Barrow/barrow.py
+++ Barrow/barrow.py
@@ -9,64 +9,77 @@ from datetime import datetime
 geonodePath = os.environ['GEONODE_INSTALL_PATH']
 geonodeVenv = os.environ['GEONODE_VIRTUALENV_PATH']
 workingDir = os.environ['DATA_WORKING_DIRECTORY']
-
 jsonFeed = 'http://feeder.gina.alaska.edu/radar-uaf-barrow-seaice-geotif.json'
-rawGeoTiff = workingDir + '/barrow_raw.tif'
-processedGeoTiff = workingDir + '/barrow_sea_ice_radar.tif'
 
 # Download and parse GINA's Barrow sea ice GeoTIFF feed.
 # The first element in the feed is the most recent GeoTIFF.
 response = urllib2.urlopen(jsonFeed)
 geoTiffs = json.loads(response.read())
-latestGeoTiff = geoTiffs[0]
-
-# Grab the latest GeoTIFF's creation date, throwing out the time zone because
-# strptime() is not able to parse the time zone in this format.
-match = re.search('^.*(?=-0[8-9]:00$)', latestGeoTiff['event_at'])
-dateObject = datetime.strptime(match.group(0), '%Y-%m-%dT%H:%M:%S')
 
 # Format the date as needed for GeoNode's importlayers management command.
-dateString = dateObject.strftime('%Y-%m-%d %H:%M:%S')
+def formatDate(geoTiffDate):
+    # Grab the latest GeoTIFF's creation date, throwing out the time zone because
+    # strptime() is not able to parse the time zone in this format.
+    match = re.search('^.*(?=-0[8-9]:00$)', geoTiffDate)
+    dateObject = datetime.strptime(match.group(0), '%Y-%m-%dT%H:%M:%S')
+
+    return dateObject.strftime('%Y-%m-%d %H:%M:%S')
 
 # Download and save the GeoTIFF file.
-geoTiffUrl = latestGeoTiff['source']
-response = urllib2.urlopen(geoTiffUrl)
-localFile = open(rawGeoTiff, 'wb')
-localFile.write(response.read())
-localFile.close()
+def download(geoTiffUrl):
+    rawGeoTiff = workingDir + '/barrow_raw.tif'
+    response = urllib2.urlopen(geoTiffUrl)
+    localFile = open(rawGeoTiff, 'wb')
+    localFile.write(response.read())
+    localFile.close()
+    return rawGeoTiff
 
 # Georeference the GeoTIFF in EPSG:3857 and make the background transparent.
-subprocess.call([
-  'gdalwarp',
-  '-s_srs',
-  '+proj=aeqd +lat_0=71.2925 +lon_0=-156.788333333333 +x_0=0 +y_0=0 +a=6358944.3 +b=6358944.3 +units=m +no_defs',
-  '-t_srs',
-  'EPSG:3857',
-  '-of',
-  'GTiff',
-  '-srcnodata',
-  '0',
-  '-dstalpha',
-  rawGeoTiff,
-  processedGeoTiff
-])
+def process(rawGeoTiff):
+    processedGeoTiff = workingDir + '/barrow_sea_ice_radar.tif'
 
-geonodeVenvPython = geonodeVenv + '/bin/python'
+    subprocess.call([
+      'gdalwarp',
+      '-s_srs',
+      '+proj=aeqd +lat_0=71.2925 +lon_0=-156.788333333333 +x_0=0 +y_0=0 +a=6358944.3 +b=6358944.3 +units=m +no_defs',
+      '-t_srs',
+      'EPSG:3857',
+      '-of',
+      'GTiff',
+      '-srcnodata',
+      '0',
+      '-dstalpha',
+      rawGeoTiff,
+      processedGeoTiff
+    ])
+
+    return processedGeoTiff
 
 # Import the GeoTIFF over the existing Barrow sea ice layer in GeoNode, and
 # update its publication date with the GeoTIFF's creation date.
-subprocess.call([
-  geonodeVenvPython,
-  geonodePath + '/geonode/manage.py',
-  'importlayers',
-  '-d',
-  dateString,
-  '-t',
-  'Barrow sea ice radar', 
-  '-o',
-  processedGeoTiff
-])
+def importLayer(processedGeoTiff, formattedDate, sequenceNumber):
+    geonodeVenvPython = geonodeVenv + '/bin/python'
 
-# Clean up temporary files so we don't get warnings from GDAL in the future.
-os.remove(rawGeoTiff)
-os.remove(processedGeoTiff)
+    subprocess.call([
+      geonodeVenvPython,
+      geonodePath + '/geonode/manage.py',
+      'importlayers',
+      '-d',
+      formattedDate,
+      '-t',
+      'Barrow sea ice radar ' + str(sequenceNumber),
+      '-o',
+      processedGeoTiff
+    ])
+
+for i in range(0, 3):
+    geoTiff = geoTiffs[i]
+
+    formattedDate = formatDate(geoTiff['event_at'])
+    rawGeoTiff = download(geoTiff['source'])
+    processedGeoTiff = process(rawGeoTiff)
+    importLayer(processedGeoTiff, formattedDate, i + 1)
+
+    # Clean up temporary files so we don't get warnings from GDAL in the future.
+    os.remove(rawGeoTiff)
+    os.remove(processedGeoTiff)
