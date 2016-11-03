8c37bc96a209ef35cf483e2a4804601a57266d71
Bob Torgerson <rltorgerson@alaska.edu>
2016-11-03 14:53:18 -0800

    Additions to prevent script being run by root or sudo.

--- update_mapventure.sh
+++ update_mapventure.sh
@@ -8,6 +8,12 @@ export MAPVENTURE_DIST="/var/www/mapventure-dist"
 BRANCH_NAME=$1
 BUILD_DIR="/tmp"
 
+if [ `whoami` == "root" ]
+        then
+                echo "Do not run this as the root user or using sudo. You will be prompted for sudo should it be required by the script."
+                exit 1
+fi
+
 if [ -z $BRANCH_NAME ]
         then
                 echo "Branch name not specified, exiting."
@@ -25,8 +31,8 @@ cd mapventure
 git pull
 git checkout $BRANCH_NAME
 
-npm install && grunt build --force --dist
+npm install && grunt build --force && grunt build --dist
 
-sudo rm -rf /tmp/mapventure-dist && sudo mv $MAPVENTURE_DIST /tmp/mapventure-dist && sudo cp -r dist $MAPVENTURE_DIST && sudo chown -R www-data:www-data $MAPVENTURE_DIST && sudo ln -s /var/www/downloads /var/www/mapventure-dist/downloads
+sudo rm -rf /tmp/mapventure-dist && sudo mv $MAPVENTURE_DIST /tmp/mapventure-dist && sudo cp -r dist $MAPVENTURE_DIST && sudo chown -R www-data:www-data $MAPVENTURE_DIST && sudo ln -s /var/www/downloads /var/www/mapventure-dist/downloads && sudo rm -rf /tmp/mapventure
 
 sudo service apache2 restart
